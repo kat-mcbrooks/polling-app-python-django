79b01c64a2b31a747a22b7dd5a5b894c52ce2cc6
kat <katharinebrooks@hotmail.co.uk>
2022-03-28 15:20:51 +0100

    implemented ChoiceInline class which extends TabularInline class to improve the admin interface so that choices can be viewed/accessed from the Question page

--- mypoll/polls/admin.py
+++ mypoll/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+# we use TabularInline so that the choices show with the questions within our admin. ChoiceInline extends from TabularInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  # how many extra fields do we want
+
+
+class QuestionAdmin(admin.ModelAdmin):  # extend ModelAdmin
+    # fieldsets is a list of tuples, which is why there is a comma at the end. In each , first thing is name (as we don't need one for question_text, its None), 2nd thing is a python dictionary
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
