8cb148aa246de541d4f418869e4a46652fd2ec9b
kat <katharinebrooks@hotmail.co.uk>
2022-03-28 17:02:11 +0100

    error in index html fixed

--- mypoll/polls/views.py
+++ mypoll/polls/views.py
@@ -6,5 +6,5 @@ from .models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]  # 5 questions, descending order by date
     # in order to pass in the questions to the template, we use a dictionary. Common convention is to call this variable context
-    context = {"latest_question_list: latest_question_list"}
+    context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
