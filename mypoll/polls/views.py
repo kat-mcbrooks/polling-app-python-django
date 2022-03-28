4008be9e7aaa3cada3360b7ccc0f6ecaf1a992b5
kat <katharinebrooks@hotmail.co.uk>
2022-03-28 15:44:07 +0100

    setup the index view in views.py and the polls route that renders index in urls.py

--- mypoll/polls/views.py
+++ mypoll/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Question, Choice
+
+# get questions and display them
+def index(request):
+    return render(request, "polls/index.html")
+
+
 # Create your views here.
