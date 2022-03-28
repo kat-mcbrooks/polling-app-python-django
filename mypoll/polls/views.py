b9c0dfcfe58d7b59fd0d00237f73349d9e6d83ab
kat <katharinebrooks@hotmail.co.uk>
2022-03-28 16:53:42 +0100

    boilerplate HTML setup in a base.html file which is extended in other html templates

--- mypoll/polls/views.py
+++ mypoll/polls/views.py
@@ -4,7 +4,7 @@ from .models import Question, Choice
 
 # get questions and display them
 def index(request):
-    return render(request, "polls/index.html")
-
-
-# Create your views here.
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]  # 5 questions, descending order by date
+    # in order to pass in the questions to the template, we use a dictionary. Common convention is to call this variable context
+    context = {"latest_question_list: latest_question_list"}
+    return render(request, "polls/index.html", context)
